da82d4278ba9bf2d3085ea2090e04362039939eb
idev2580 <idev2580@gmail.com>
2025-02-11 16:51:48 +0900

    Implement BloodOxygenCollector(Not tested)

--- app/src/main/java/com/example/trackertest/tracker/collector/samsunghealth/BloodOxygenCollector.kt
+++ app/src/main/java/com/example/trackertest/tracker/collector/samsunghealth/BloodOxygenCollector.kt
@@ -10,12 +10,18 @@ import com.example.trackertest.tracker.data.SingletonStorageInterface
 import com.example.trackertest.tracker.permission.PermissionManagerInterface
 import com.samsung.android.sdk.health.data.HealthDataService
 import com.samsung.android.sdk.health.data.HealthDataStore
+import com.samsung.android.sdk.health.data.data.ChangeType
+import com.samsung.android.sdk.health.data.data.entries.OxygenSaturation
+import com.samsung.android.sdk.health.data.request.DataType
+import com.samsung.android.sdk.health.data.request.DataTypes
+import com.samsung.android.sdk.health.data.request.InstantTimeFilter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.lang.Thread.sleep
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
 
@@ -64,8 +70,61 @@ class BloodOxygenCollector(
 
     var lastSyncTimestamp:Long = -1L
     suspend fun readAllData(store: HealthDataStore):List<Pair<MetadataEntity, List<Entity>>>{
-        //TODO
-        return listOf()
+        val timeFilter = InstantTimeFilter.since(Instant.ofEpochMilli(lastSyncTimestamp + 1))
+        val req = DataTypes.BLOOD_OXYGEN
+            .changedDataRequestBuilder
+            .setChangeTimeFilter(timeFilter)
+            .build()
+
+        lastSyncTimestamp = System.currentTimeMillis()
+        val dataList = store.readChanges(req).dataList
+
+        val retList:MutableList<Pair<MetadataEntity, List<Entity>>> = mutableListOf()
+        dataList.forEach{
+            if(it.changeType == ChangeType.UPSERT){
+                //TODO
+                val uid:String = it.upsertDataPoint.uid
+                val startTime:Long = it.upsertDataPoint.startTime.toEpochMilli()
+                val endTime:Long? = it.upsertDataPoint.endTime?.toEpochMilli()
+                val oxygenSaturation:Float? = it.upsertDataPoint.getValue(DataType.BloodOxygenType.OXYGEN_SATURATION)
+                val max:Float? = it.upsertDataPoint.getValue(DataType.BloodOxygenType.MAX_OXYGEN_SATURATION)
+                val min:Float? = it.upsertDataPoint.getValue(DataType.BloodOxygenType.MIN_OXYGEN_SATURATION)
+
+                val entityList:MutableList<Entity> = mutableListOf()
+                val measurements:List<OxygenSaturation>? = it.upsertDataPoint.getValue(DataType.BloodOxygenType.SERIES_DATA)
+                measurements?.forEach{
+                    val m_uid:String = uid
+                    val m_startTime:Long = it.startTime.toEpochMilli()
+                    val m_endTime:Long = it.endTime.toEpochMilli()
+                    val m_min:Float = it.min
+                    val m_max:Float = it.max
+                    val m_oxygenSaturation:Float = it.oxygenSaturation
+
+                    entityList.add(Entity(
+                        System.currentTimeMillis(),
+                        m_uid,
+                        m_startTime,
+                        m_endTime,
+                        m_min,
+                        m_max,
+                        m_oxygenSaturation
+                    ))
+                }
+
+                retList.add(
+                    Pair(
+                        MetadataEntity(
+                            System.currentTimeMillis(),
+                            uid,
+                            startTime,
+                            endTime?:startTime,
+                            oxygenSaturation ?:Float.NaN
+                        ), entityList
+                    )
+                )
+            }
+        }
+        return retList
     }
 
     override fun start() {
@@ -101,8 +160,7 @@ class BloodOxygenCollector(
         val uid : String,
         val startTime : Long,
         val endTime : Long,
-        val oxygenSaturation: Float,
-        val measurementType: String
+        val oxygenSaturation: Float
         //val appId: String,
         //val deviceId: String
     ) : DataEntity(received)
